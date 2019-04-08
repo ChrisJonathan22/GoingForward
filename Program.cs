ffa888da4d57f56b7a7b8c7589561ccb37b241c4
Chris Jonathan <christophereko@hotmail.fr>
2019-04-08 19:30:51 +0100

    I've added messages to tell the player how to play the game and what is required in order to win. I've added an option to allow the player to exit when and if needed. Every time that a user inputs the right number they advance forward and when they get it wrong they go back one step.

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ namespace GoingForward
             // Ask the player if they are ready to play
             Console.WriteLine("Are you ready to play? [ Y | N ]");
 
-             // Reset the console's text color
+            // Reset the console's text color
             Console.ResetColor();
             
             // Store the user's answer
@@ -27,6 +27,45 @@ namespace GoingForward
             // If the player is ready to play log the following
             if(userAnswer == "Y") {
                 Console.WriteLine("Great!");
+
+                int playerMoves = 0;
+
+                while (playerMoves < 5) {
+                    // Generate a random number between 0 & 2
+                    int rand = CreateRandomNumber.GenerateRandomNumber();
+
+                    // Tell the player what they need to do in order to win
+                    Console.WriteLine("To win the game you need to reach level 5 by entering 5 correct numbers in a row.");
+
+                    // Ask the player to enter a number
+                    Console.WriteLine("Please enter a number between 0 & 2");
+
+                    // Give the player instructions on what to do in order to exit
+                    Console.WriteLine("To exit the game simply type in 3 and enter");
+
+                    // Convert the player's input to an int and store it
+                    int playerInput = Int32.Parse(Console.ReadLine());
+
+                    // If the player enters a number which matches the random number then do this
+                    if(rand == playerInput) {
+                        playerMoves += 1;
+                        Console.WriteLine($"You're on level {playerMoves}");
+                            if(playerMoves == 10) {
+                                Console.WriteLine($"Congratulations you've reached level {playerMoves}!");
+                            }
+                    }
+
+                    // If the player enters 3 return / exit the game
+                    else if(playerInput == 3) {
+                        return;
+                    }
+
+                    // If the player's number doesn't match the random number then do this
+                    else {
+                        playerMoves = playerMoves == 0 ? 0 : playerMoves - 1;
+                        Console.WriteLine($"You're on level {playerMoves}");
+                    }
+            }
             }
             // If the player isn't ready to play log the following
             else {
@@ -77,6 +116,19 @@ namespace GoingForward
 
         // Reset the console's text color
         Console.ResetColor(); 
+        }
+    }
+
+    public class CreateRandomNumber {
+        public static int GenerateRandomNumber () {
+            // Create a new random object
+            Random random = new Random();
+
+            // Get a random number between 0 & 2
+            int randomNumber = random.Next(0,3);
+
+            // Return random number
+            return randomNumber;
         }
     }
 }
